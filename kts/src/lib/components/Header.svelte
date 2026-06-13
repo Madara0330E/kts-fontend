9dd4b5e8acd883aefb84dee840e352f3e48f761f
MarshmallÓws <157970838+Madara0330E@users.noreply.github.com>
2026-06-13 23:34:06 +0500

    Add header logos and update background color
    Add four image assets and integrate them into Header.svelte: import kts, slesh, and spm images and render them in a centered logo group. Also adjust global body background color in layout.css from #020617 to #212121 to match the updated theme.

--- kts/src/lib/components/Header.svelte
+++ kts/src/lib/components/Header.svelte
@@ -1,9 +1,23 @@
 <script>
+  import ktsImg from '$lib/assets/images/components/kts.png';
+  import sleshImg from '$lib/assets/images/components/slesh.png';
+  import spmhImg from '$lib/assets/images/components/spm.png';
   let { title = 'KTS Frontend' } = $props();
 </script>
 
 <header class="bg-[#0f1015] 5 ">
   <div class="mx-auto flex max-w-screen-xl items-center justify-between h-[72px]">
- 
+  <div class="justify-center flex w-[182px] gap-[4px] h-[42px] items-center ">
+  <div class="div">
+    <img src={ktsImg} alt="kts" class="w-[76px] h-[28px]" />
+  </div>
+  <div class="div ">
+      <img src={sleshImg} alt="slesh" class="w-[12px]  h-[42px]" />
+  </div>
+  <div class="div">
+      <img src={spmhImg} alt="spm" class="w-[86px] h-[36px]" />
+  </div>
+  </div>
+  
   </div>
 </header>
