f0f47c370207ed877df17c38d3b26b0cb21cfbae
idv <git.mrezhuk@gmail.com>
2026-06-17 20:46:46 +0300

    add zaralx api

--- kts/openapi-gen.sh
+++ kts/openapi-gen.sh
@@ -1,1 +1,2 @@
-npx openapi-typescript https://kts-development.mzh.su/swagger/openapi.json -o src/lib/api/schema.d.ts
+npx openapi-typescript https://kts-development.mzh.su/swagger/openapi.json -o src/lib/api/kts/schema.d.ts
+npx openapi-typescript https://assets.zaralx.ru/api/swagger -o src/lib/api/zaralx/schema.d.ts
--- kts/src/routes/profile/+page.svelte
+++ kts/src/routes/profile/+page.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
-    import { client } from "$lib/api/client";
-    import { API_CHAIN } from "$lib/api/client";
+    import { client } from "$lib/api/kts/client";
+    import { API_CHAIN } from "$lib/api/kts/client";
+    import { client as zaralx } from "$lib/api/zaralx/client";
+
     const promise = client.GET("/users/get/{id}", { params: { path: { id: API_CHAIN } } });
+    const image = zaralx.GET("/v1/minecraft/vanilla/item/{item}/icon", {
+        params: {
+            path: {
+                item: "golden_apple"
+            }
+        },
+        parseAs: "blob"
+    });
 </script>
 
 <div>
@@ -11,5 +21,16 @@
     <p>{JSON.stringify(result.data)} {result.error}</p>
 {:catch error}
     <p>Error: {JSON.stringify(error)}</p>
+{/await}
+
+{#await image}
+    <p>Loading...</p>
+{:then result}
+    <p>{result.error}</p>
+    {#if result.data}
+    <img src={URL.createObjectURL(result.data)} alt="golden_apple">
+    {/if}
+{:catch error}
+    <p>Error: {error}</p>
 {/await}
 </div>
