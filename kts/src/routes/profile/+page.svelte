7d834c0a1207813a9d6c374ac79afb8cd48705a5
idv <git.mrezhuk@gmail.com>
2026-06-17 19:54:32 +0300

    add async example

--- kts/src/routes/profile/+page.svelte
+++ kts/src/routes/profile/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
     import { client } from "$lib/api/client";
     import { API_CHAIN } from "$lib/api/client";
-
-    const { data, error } = await client.GET("/users/get/{id}", { params: { path: { id: API_CHAIN } } });
+    const promise = client.GET("/users/get/{id}", { params: { path: { id: API_CHAIN } } });
 </script>
-    <p>{JSON.stringify(data)} {error}</p>
+
 <div>
+{#await promise}
+    <p>Loading...</p>
+{:then result}
+    <p>{JSON.stringify(result.data)}</p>
+{:catch error}
+    <p>Error: {error}</p>
+{/await}
 </div>
