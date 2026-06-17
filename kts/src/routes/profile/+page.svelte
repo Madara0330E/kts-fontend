17cbb0e8dbe0af747dde66415921f8cc1a44bb1a
idv <git.mrezhuk@gmail.com>
2026-06-17 19:48:54 +0300

    add open api generated api

--- kts/src/routes/profile/+page.svelte
+++ kts/src/routes/profile/+page.svelte
@@ -1,5 +1,9 @@
-<script>
-</script>
+<script lang="ts">
+    import { client } from "$lib/api/client";
+    import { API_CHAIN } from "$lib/api/client";
 
+    const { data, error } = await client.GET("/users/get/{id}", { params: { path: { id: API_CHAIN } } });
+</script>
+    <p>{JSON.stringify(data)} {error}</p>
 <div>
 </div>
