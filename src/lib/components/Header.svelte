6b4ff5fe67368e2fdf11cf04cda0b5bb5486b31d
MarshmallÓws <157970838+Madara0330E@users.noreply.github.com>
2026-06-19 22:16:06 +0500

    adaptiv
    adaptiv

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -13,26 +13,27 @@
 	];
 </script>
 
-<header class="bg-[#212121] w-full h-[6.667vmin] flex items-center justify-center pl-[2.778vmin] pr-[2.778vmin]">
-	<div class="flex items-center justify-between h-[6.667vmin] w-[142.963vmin]">
-		<div class="justify-center flex w-[16.852vmin] h-[3.889vmin] items-center">
-			<div><img src={ktsImg} alt="kts" class="w-[7.037vmin] h-[2.593vmin]" /></div>
-			<div><img src={sleshImg} alt="slesh" class="w-[1.111vmin] h-[3.889vmin]" /></div>
-			<div><img src={spmhImg} alt="spm" class="w-[7.963vmin] h-[3.333vmin]" /></div>
+<header class="bg-[#212121] w-full h-[6.667vmin] lg:h-[72px] flex items-center justify-center pl-[9.792vmin] pr-[9.792vmin]">
+	<div class="flex items-center justify-between h-[6.667vmin] lg:h-[72px] w-[142.963vmin] lg:w-full lg:max-w-screen-xl">
+		
+		<div class="justify-center flex w-[16.852vmin] lg:w-[182px] h-[3.889vmin] lg:h-[42px] items-center">
+			<div><img src={ktsImg} alt="kts" class="w-[7.037vmin] lg:w-[76px] h-[2.593vmin] lg:h-[28px]" /></div>
+			<div><img src={sleshImg} alt="slesh" class="w-[1.111vmin] lg:w-[12px] h-[3.889vmin] lg:h-[42px]" /></div>
+			<div><img src={spmhImg} alt="spm" class="w-[7.963vmin] lg:w-[86px] h-[3.333vmin] lg:h-[36px]" /></div>
 		</div>
 
-		<nav class="flex items-center justify-between h-[6.667vmin] w-[37.222vmin]">
+		<nav class="flex items-center justify-between h-[6.667vmin] lg:h-[72px] w-[37.222vmin] lg:w-auto lg:gap-[64px]">
 			{#each navItems as item}
 				<a href={item.href} class="flex items-center">
-					<span class="text-[1.852vmin] text-[#F8F8F8] font-medium font-tiktok">{item.name}</span>
+					<span class="text-[1.852vmin] lg:text-xl text-[#F8F8F8] font-medium font-tiktok">{item.name}</span>
 				</a>
 			{/each}
 		</nav>
 
-		<div class="profile gap-[1.111vmin] flex items-center justify-center">
+		<div class="profile gap-[1.111vmin] lg:gap-3 flex items-center justify-center">
 			<div>
 				<a href="/profile" class="flex items-center justify-center">
-					<span class="text-[1.852vmin] text-[#F8F8F8] font-medium font-tiktok">
+					<span class="text-[1.852vmin] lg:text-xl text-[#F8F8F8] font-medium font-tiktok">
 						{$userStore?.nickname ?? 'Гость'}
 					</span>
 				</a>
@@ -43,10 +44,10 @@
 						<img 
 							src={`https://assets.zaralx.ru/api/v1/minecraft/vanilla/player/face/${$userStore.nickname}`}
 							alt={$userStore.nickname} 
-							class="w-[4.074vmin] h-[4.074vmin] object-cover rounded-sm" 
+							class="w-[4.074vmin] lg:w-[44px] h-[4.074vmin] lg:h-[44px] object-cover rounded-sm" 
 						/>
 					{:else}
-						<div class="w-[4.074vmin] h-[4.074vmin] bg-gray-600 rounded-sm animate-pulse"></div>
+						<div class="w-[4.074vmin] lg:w-[44px] h-[4.074vmin] lg:h-[44px] bg-gray-600 rounded-sm animate-pulse"></div>
 					{/if}
 				</a>
 			</div>
--- src/routes/banking/+page.svelte
+++ src/routes/banking/+page.svelte
@@ -9,11 +9,11 @@
 </script>
 
 <div>
-  <div class="w-full h-full max-w-full max-h-full flex flex-col gap-[14px] bg-[#212121]/60 pl-[8px] pr-[8px] rounded-[20px] pt-[8px] pb-[8px]">
-    <div class="w-full h-full max-w-full max-h-full flex justify-center items-center bg-[#212121] pl-[8px] pr-[8px] pt-[4px] pb-[4px] gap-[4px] rounded-[20px] ">
-     <div class="flex items-center justify-center rounded-[12px] bg-[#2C2C2C] w-[45px] h-[45px]">  <img src={leftArrowImg} alt="leftArrow" class="" /></div>
-     <div class="flex items-center justify-center rounded-[12px] bg-[#2C2C2C] w-[45px] h-[45px]">  <img src={rightArrowImg} alt="rightArrow" class="" /></div>
-    <div class="pt-[14.5px] pb-[14.5px] pl-[18px] pr-[18px] flex items-center w-[1020px] h-[45px] px-[8px] gap-[8px] rounded-[12px] border bg-[#F8F8F8]/5 focus:outline-none focus:ring-0 border-none placeholder:font-tiktok placeholder:text-[16px] ">
+  <div class="w-full h-full max-w-[1264px] max-h-full flex flex-col gap-[14px] bg-[#212121]/60 pl-[8px] pr-[8px] rounded-[20px] pt-[8px] pb-[8px]">
+    <div class="w-full h-full max-w-[1248px] max-h-full flex justify-center items-center bg-[#212121] pl-[8px] pr-[8px] pt-[4px] pb-[4px] gap-[4px] rounded-[20px] gap-[8px] ">
+     <div class="flex items-center justify-center rounded-[12px] bg-[#2C2C2C] min-w-[45px] h-[45px]">  <img src={leftArrowImg} alt="leftArrow" class="" /></div>
+     <div class="flex items-center justify-center rounded-[12px] bg-[#2C2C2C] min-w-[45px] h-[45px]">  <img src={rightArrowImg} alt="rightArrow" class="" /></div>
+    <div class="pt-[14.5px] pb-[14.5px] pl-[18px] pr-[18px] flex items-center w-full max-w-[1020px] h-[45px] px-[8px] gap-[8px] rounded-[12px] border bg-[#F8F8F8]/5 focus:outline-none focus:ring-0 border-none placeholder:font-tiktok placeholder:text-[16px] ">
       
       <img src={magnifiersImg} alt="magnifier" class="w-[16px] h-[16px]" />
       
@@ -24,11 +24,11 @@
         class="w-full h-full bg-transparent text-[#F8F8F8]/30 text-[16px] font-medium font-tiktok focus:outline-none focus:ring-0 border-none placeholder:font-tiktok placeholder:text-[16px]"
       />
     </div>
-    <div class="flex items-center justify-center rounded-[12px] bg-[#2C2C2C] w-[45px] h-[45px]">  <img src={filterParametersImg} alt="filterParameters" class="" /></div>
-    <div class="flex items-center justify-center rounded-[12px] bg-[#2C2C2C] w-[45px] h-[45px]">  <img src={sortingOptionsImg} alt="sortingOptions" class="" /></div>
+    <div class="flex items-center justify-center rounded-[12px] bg-[#2C2C2C] min-w-[45px] h-[45px]">  <img src={filterParametersImg} alt="filterParameters" class="" /></div>
+    <div class="flex items-center justify-center rounded-[12px] bg-[#2C2C2C] min-w-[45px] h-[45px]">  <img src={sortingOptionsImg} alt="sortingOptions" class="" /></div>
     </div>
 
-    <div class="flex gap-[10px]">
+    <div class="flex flex-wrap gap-[10px]">
       <Cards />
       <AddCard />
     </div>
